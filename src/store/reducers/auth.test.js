a000dd437d16c964d09a8263c12c7bb632b2be22
Luka Kajtes <luka.kajtes@synechron.com>
2019-08-20 14:20:02 +0200

    Unit tests jest+enzyme

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,24 @@
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+
+    it('should store tokern upon login', () => {
+        expect(reducer(undefined, {type: actionTypes.AUTH_SUCCESS, idToken: 'some token', userId: 'some userId'})).toEqual({
+            token: 'some token',
+            userId: 'some userId',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        })
+    });
+});
